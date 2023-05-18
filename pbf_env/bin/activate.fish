96b5c81bb803bf44a57a5c72d7865f92d2e0f7b6
TyHanks <thanks10414@gmail.com>
2023-05-18 00:39:28 -0400

    All base files for django minus env

--- pbf_env/bin/activate.fish
+++ pbf_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/thanksdev/Documents/Projects/pbf-blog/pbf_env"
+set -gx VIRTUAL_ENV "/home/thanksdev/Documents/Projects/PBF-Blog/pbf_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
